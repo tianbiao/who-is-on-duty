cc10088cd6cccd6bd8d79faff2929fbe607fa0d3
Biao Tian <btian@thoughtworks.com>
2020-04-22 20:57:10 +0800

    update activity Participators

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -6,18 +6,22 @@ Page({
     team: '',
     activity: '',
     participators: [],
+    teamMembers: [],
     index: 0,
     disabled: true
   },
   onLoad: function (options) {
     const team = app.globalData.ondutyData.find(t => t.name === options.team)
     const activity = team.activities.find(a => a.name === options.activity)
+    
     this.setData({
       team: team,
       activity: activity,
       participators: activity.participators,
+      teamMembers: team.members.map(member => ({name: member, isParticipator: activity.participators.includes(member)})),
       index: activity.participators.findIndex(e => e === activity.onDutyUser)
     })
+
     console.log('333:', this.data.team)
   },
   changeOnDutyUser: function(e) {
@@ -31,14 +35,26 @@ Page({
       })
     }
   },
+  changedParticipators: function(e) {
+    console.log('changedParticipators checkBox:', e.detail.value)
+    const currentOnDutyUser = this.data.participators[this.data.index]
+    const currentOnDutyUserIndex = e.detail.value.findIndex(user => user === currentOnDutyUser);
+    this.setData({
+      participators: e.detail.value,
+      index: currentOnDutyUserIndex < 0 ? 0 : currentOnDutyUserIndex,
+      disabled: e.detail.value.length == 0
+    })
+  },
   save: function() {
     const activities = this.data.team.activities.map(a => {
       if(a.name == this.data.activity.name) {
         a.onDutyUser = this.data.participators[this.data.index]
+        a.participators = this.data.participators
       }
       return a
     })
     console.log('save activities: ', activities)
+
     db.collection('teams').doc(this.data.team._id).update({
       data: {
         activities: activities
