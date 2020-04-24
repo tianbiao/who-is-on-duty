92b13c09aeb9feb66862caf2aa2ab4a1833b5a76
Ying Zhou <yingzhou@thoughtworks.com>
2020-04-24 18:45:50 +0800

    improve ui style (function not finished)

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -8,12 +8,21 @@ Page({
     participators: [],
     teamMembers: [],
     index: 0,
-    disabled: true
+    disabled: true,
+    bgSize: {},
+    flexWrapFix: true,
+    bgimgList: [
+      'https://goss1.veer.com/creative/vcg/veer/612/veer-310544626.jpg',
+      'https://goss1.veer.com/creative/vcg/veer/612/veer-309964695.jpg',
+      'https://goss.veer.com/creative/vcg/veer/612/veer-167598354.jpg',
+      'https://goss1.veer.com/creative/vcg/veer/612/veer-153462540.jpg',
+      'https://goss4.veer.com/creative/vcg/veer/612/veer-158618183.jpg',
+    ]
   },
   onLoad: function (options) {
     const team = app.globalData.ondutyData.find(t => t.name === options.team)
     const activity = team.activities.find(a => a.name === options.activity)
-    
+
     this.setData({
       team: team,
       activity: activity,
@@ -21,6 +30,7 @@ Page({
       teamMembers: team.members.map(member => ({name: member, isParticipator: activity.participators.includes(member)})),
       index: activity.participators.findIndex(e => e === activity.onDutyUser)
     })
+    this.styleAdjustment();
   },
   changeOnDutyUser: function(e) {
     this.setData({
@@ -61,5 +71,25 @@ Page({
         })
       }
     })
+  },
+  styleAdjustment: function () {
+    const query = wx.createSelectorQuery();
+    query.select('.team-members').boundingClientRect();
+    query.selectAll('.team-member').boundingClientRect();
+    query.select('#flex-wrap-fix').boundingClientRect();
+    query.exec(([containerBounding, memberBounding, flexWrapFix]) => {
+      flexWrapFix = flexWrapFix.left > memberBounding[0].left;
+      const offset = (containerBounding.width * 1.15 - containerBounding.height) / 2;
+      this.setData({
+        flexWrapFix,
+        bgSize: `${containerBounding.width}px`,
+        teamMembers: this.data.teamMembers.map(
+          (member, idx) => ({
+            ...member,
+            x: containerBounding.left - memberBounding[idx].left,
+            y: containerBounding.top - memberBounding[idx].top - offset,
+          })),
+      });
+    });
   },
 })
